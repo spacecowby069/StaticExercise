7ac1399af8d28ea6832d73483b78649b8b457687
Michael Farris <spacecowby069@yahoo.com>
2021-03-05 17:22:47 -0500

    My Final Commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userOption = null;
+            do
+            {
+                Console.WriteLine("What conversion would you like to perform? FtoC or CtoF?");
+                userOption = Console.ReadLine().ToLower();
+
+                if (userOption == "ftoc")
+                {
+                    Console.WriteLine("What is the Fahrenheit that you want to convert to Celsius?");
+                    double fahTemp = Convert.ToDouble(Console.ReadLine());
+
+                    TempConverter.FahrenheitToCelsius(fahTemp);
+
+                    double convertToCel = TempConverter.FahrenheitToCelsius(fahTemp);
+
+                    Console.WriteLine();
+                    Console.WriteLine($"{fahTemp} is {convertToCel} degrees Celsius.");
+                }
+                else if (userOption == "ctof")
+                {
+                    Console.WriteLine("What temperature in Celsius do you want to convert to Fahrenheit?");
+                    double celTemp = Convert.ToDouble(Console.ReadLine());
+
+                    double convertToFah = TempConverter.CelsiusToFarenheit(celTemp);
+
+                    Console.WriteLine();
+                    Console.WriteLine($"{celTemp} is {convertToFah} degrees Fahrenheit.");
+                }
+                else
+                {
+                    Console.WriteLine("Did not recognize that command. please try again");
+                }
+
+                Console.WriteLine("If you would like to quit, type exit. ");
+                userOption = Console.ReadLine();
+
+                
+                
+
+                
+
+                
+            } while (userOption != "exit");
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        static TempConverter()
+        {
+        }
+
+        public static double FahrenheitToCelsius(double fTemp)
+        {
+            var celTemp = ((fTemp - 32)) / 1.8;
+            return celTemp;
+        }
+
+        public static double CelsiusToFarenheit(double cTemp)
+        {
+            var fahTemp = ((cTemp * 1.8)) + 32;
+            return fahTemp;
+        }
+    }
+}
